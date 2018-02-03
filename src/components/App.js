93be347e678d65127eff99ff38c9dfd738943a1d
Anshul <anshulchanchlani@gmail.com>
2018-02-03 15:15:38 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,60 @@ import React from 'react';
 import LeftNav from './LeftNav';
 import MainBody from './MainBody';
 export default class App extends React.Component{
-    changeMainBodyChild(){
-        
+  constructor(props){
+    super(props)
+    this.state ={
+        views:{
+                isAddRecipe:false,
+                isRemoveRecipe:false,
+                isEditRecipe:false
+              }
+         };
+      this.changeMainBodyChildren: this.changeMainBodyChildren.bind(this);
     }
+    
+  changeMainBodyChildren(value){
+    var self = this;
+    switch(value){
+        case 'isAddRecipe': self.setState({
+              
+                                      views:{
+                                             isAddRecipe:true,
+                                             isRemoveRecipe:false,
+                                             isEditRecipe:false
+                                            }
+               
+                                        });
+                                        break;
+        case 'isRemoveRecipe': self.setState({
+              
+                                      views:{
+                                             isAddRecipe:false,
+                                             isRemoveRecipe:true,
+                                             isEditRecipe:false
+                                            }
+               
+                                        });
+                                        break;
+         case 'isEditRecipe': self.setState({
+              
+                                      views:{
+                                             isAddRecipe:false,
+                                             isRemoveRecipe:false,
+                                             isEditRecipe:true
+                                            }
+               
+                                        });
+                                        break;
+                            
+    }  
+  }
     render()
     {
         return(
         <div>
-         <LeftNav/>
-         <MainBody/>
+         <LeftNav changeMainBodyChildren ={this.changeMainBodyChildren}/>
+         <MainBody viewState = {this.state.views}/>
             </div>
         );
     }
